fd83c6b7b7e8583e5421f45790f977517c244f21
Shivam-yadav0206 <92823134+Shivam-yadav0206@users.noreply.github.com>
2024-01-24 21:48:16 +0530

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBwDtZFVzDEJ6aChwmZT8-DQmeJVe6A378",
-  authDomain: "upraised-ca971.firebaseapp.com",
-  projectId: "upraised-ca971",
-  storageBucket: "upraised-ca971.appspot.com",
-  messagingSenderId: "705258526953",
-  appId: "1:705258526953:web:893e19200e5e829996f878",
-  measurementId: "G-8CSH6EHK1H",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
